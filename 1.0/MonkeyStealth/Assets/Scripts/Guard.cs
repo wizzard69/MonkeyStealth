8c6a170771094187bf3e2857f954c04f03a51acc
WizBit <dean@monkiezone.com>
2017-09-25 14:03:19 +0100

    Lunchtime Complete

--- 1.0/MonkeyStealth/Assets/Scripts/Guard.cs
+++ 1.0/MonkeyStealth/Assets/Scripts/Guard.cs
@@ -6,8 +6,10 @@ public class Guard : MonoBehaviour
 {
 
     public Transform pathHolder;
+
     public float speed = 5f;
     public float waitTime = 0.3f;
+    public float turnSpeed = 90f;
 
 
     void Start()
@@ -17,6 +19,8 @@ public class Guard : MonoBehaviour
         for (int i = 0; i < waypoints.Length; i++)
         {
             waypoints[i] = pathHolder.GetChild(i).position;
+            waypoints[i] = new Vector3(waypoints[i].x,transform.position.y,waypoints[i].z);
+
         }
 
         StartCoroutine(FollowPath(waypoints));
@@ -28,6 +32,7 @@ public class Guard : MonoBehaviour
 
         int targetWaypointIndex = 1;
         Vector3 targetWaypoint = waypoints[targetWaypointIndex];
+        transform.LookAt(targetWaypoint);
 
         while (true)
         {
@@ -38,10 +43,27 @@ public class Guard : MonoBehaviour
                 targetWaypointIndex = (targetWaypointIndex + 1) % waypoints.Length;
                 targetWaypoint = waypoints[targetWaypointIndex];
                 yield return new WaitForSeconds(waitTime);
+
+                yield return StartCoroutine(TurnToFace(targetWaypoint));
             }
 
             yield return null;
         }
+    }
+
+    IEnumerator TurnToFace(Vector3 lookTarget)
+    {
+        Vector3 dirToLookTarget = (lookTarget - transform.position).normalized;
+        float targetAngle = 90 - Mathf.Atan2(dirToLookTarget.z, dirToLookTarget.x) * Mathf.Rad2Deg;
+
+        while (Mathf.DeltaAngle(transform.eulerAngles.y,targetAngle) > 0.05f)
+        {
+            float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetAngle, turnSpeed * Time.deltaTime);
+            transform.eulerAngles = Vector3.up * angle;
+
+            yield return null;
+        }
+
     }
 
     private void OnDrawGizmos()
